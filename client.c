f06dc0884b5028df06ee519c87978cada973269d
zovivo <tmczovivo@gmail.com>
2019-12-15 12:45:27 +0700

    update

---
+++ client.c
@@ -0,0 +1,316 @@
+#include <stdio.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h> 
+
+
+#define SERVER_ADDR "127.0.0.1"
+#define SERVER_PORT 5550
+#define BUFF_SIZE 1024
+#define FIND_FILE_REQUEST "Find file"
+#define FILE_NOT_FOUND "File not found"
+#define FILE_EXIST "File exist"
+#define SEND_FILE_REQUEST "Send file"
+#define SEARCH_MODE "Search mode"
+#define SHARE_MODE "Share mode"
+
+#define STORAGE "./share/"
+
+int searchFile(char *filename);
+void sendFile(int client_sock,char *fileName);
+void receiveFile(int client_sock, char *fileName);
+
+void doSearchFile(int client_sock);
+void doSendFile(int client_sock);
+
+void searchMode(int client_sock);
+void shareMode(int client_sock);
+
+
+typedef struct tagClient
+{
+    char addressIP[256];
+    char id[20];
+    struct tagClient *next;
+} client;
+
+client *head = NULL;
+client *current = NULL;
+
+void sendFile(int client_sock, char *fileName){
+    
+    FILE *fileptr;
+    long filelen=0l;
+    int bytes_sent;
+    char name[100];
+	strcpy(name,STORAGE);
+	strcat(name,fileName);
+
+    fileptr = fopen(name, "rb");
+    fseek(fileptr, 0, SEEK_END);          // Jump to the end of the file
+    filelen = ftell(fileptr);             // Get the current byte offset in the file       
+    rewind(fileptr);
+
+    printf("%s\n", "Ham gui file");
+
+    bytes_sent = send(client_sock, &filelen, 20, 0); //send file size
+    if(bytes_sent <= 0){
+        printf("\nConnection closed!\n");
+        return;
+    }
+
+    printf("filelen: %ld\n", filelen);
+
+    int sumByte = 0; //count size byte send
+    printf("\n Bat dau doc file !\n");
+    while(1) {
+        int numberByteSend = BUFF_SIZE;
+        if((sumByte + BUFF_SIZE	) > filelen) {// if over file size
+            numberByteSend = filelen - sumByte; 
+        }
+        char* buffer = (char *) malloc((numberByteSend) * sizeof(char));
+        fread(buffer, numberByteSend, 1, fileptr); // read buffer with size 
+        sumByte += numberByteSend; //increase byte send
+        printf("%s\n", buffer);
+        bytes_sent = send(client_sock, buffer, numberByteSend, 0);
+        if(bytes_sent <= 0){
+            printf("\nConnection closed!\n");
+            break;
+        }
+        free(buffer);
+        if(sumByte >= filelen) {
+            break;
+        }
+    }
+    printf("%s\n", "Send file success !"); 
+    fclose(fileptr); //close file
+}
+
+void convertToInsertHead(char * clientStr)
+{	
+	printf("clientStr: %s\n", clientStr);
+	char *token;
+	char *ipAddress;
+	char *id;
+	char minus[] = "-";
+	client *link = (client *)malloc(sizeof(client));
+	token = strtok(clientStr,minus);
+	// if(token == NULL)
+	// 	return;
+	strcpy(link->addressIP, token);
+	printf("addressIP: %s\n", token);
+	token = strtok(NULL, minus);
+	strcpy(link->id, token);
+	printf("ID: %s\n", token);
+    link->next = head;
+    head = link;
+}
+
+void searchMode(int client_sock){
+	char filename[BUFF_SIZE];
+	int bytes_sent;
+	int bytes_received;
+	char listClientShare[BUFF_SIZE];
+	char buff[BUFF_SIZE];
+	char minus[] = "-";
+    char enter[] = "\n";
+    char *token;
+    char clientStr[256];
+    char clientID[20];
+    client *ptr;
+	bytes_sent = send(client_sock, SEARCH_MODE, BUFF_SIZE, 0);
+	if(bytes_sent < 0)
+		perror("\nError: ");
+	printf("%s\n", "Enter File name: ");
+	scanf("%s",filename);
+	// printf("File name: %s\n", filename);
+	bytes_sent = send(client_sock, filename, BUFF_SIZE, 0);
+	bytes_received =  recv(client_sock, buff, BUFF_SIZE, 0);
+	buff[bytes_received] = '\0';
+	if(strcmp(buff,FILE_NOT_FOUND) ==0 ){
+		printf("%s\n", FILE_NOT_FOUND);
+		return;
+	}
+	strcpy(listClientShare, buff);
+   	printf("%s\n", "============================");
+   	printf("%s\n", "List client has file :");
+   	printf("%s", listClientShare);
+
+   	printf("%s\n", "============================");
+   	printf("%s\n", "Enter ID Client to get file :");
+   	scanf("%s", clientID);
+   	bytes_sent = send(client_sock, clientID, BUFF_SIZE, 0);
+
+   	receiveFile(client_sock,filename);
+
+
+}
+
+void receiveFile(int client_sock, char *fileName){
+	FILE *filePtr;
+	char name[100];
+	char *fileContent;
+	int bytes_received;
+	long filelen=0l;
+
+	strcpy(name, STORAGE); 
+	strcat(name, fileName);
+
+	printf("%s\n", "Ham nhan file");
+
+	bytes_received = recv(client_sock, &filelen, 20, 0);
+	if (bytes_received <= 0){
+		printf("\nConnection closed");
+		return;
+	}
+	printf("Uploaded file: %s, Size of file: %ld\n\n", fileName, filelen);
+
+	int sumByte = 0;
+	filePtr = fopen(name, "wb");
+	fileContent = (char*) malloc(BUFF_SIZE * sizeof(char));
+	while(1) {
+		bytes_received = recv(client_sock, fileContent, BUFF_SIZE, 0);
+		if(bytes_received == 0) {
+			printf("Error: File tranfering is interupted\n\n");
+		}
+		sumByte += bytes_received;
+		fwrite(fileContent, bytes_received, 1, filePtr);
+		free(fileContent);
+		fileContent = (char*) malloc(BUFF_SIZE * sizeof(char));
+		if(sumByte >= filelen) {
+			break;
+		}
+	}
+	printf("%s\n", "Receive file success !"); 
+	fclose(filePtr);
+}
+
+void shareMode(int client_sock){
+	int bytes_sent;
+	int bytes_received;
+	char buff[BUFF_SIZE + 1];
+	char filename[BUFF_SIZE + 1];
+
+	bytes_sent = send(client_sock, SHARE_MODE, BUFF_SIZE, 0);
+	if(bytes_sent < 0)
+		perror("\nError: ");
+	printf("%s\n", "Waiting for request from server...");
+	bytes_received =  recv(client_sock, filename, BUFF_SIZE, 0);
+	filename[bytes_received] = '\0';
+	printf("Finding file : %s \n", filename);
+	if (searchFile(filename) == 1)
+	{	
+		printf("%s\n", FILE_EXIST);
+		bytes_sent = send(client_sock, FILE_EXIST, BUFF_SIZE, 0);
+		printf("Sended: %s\n", FILE_EXIST);
+		bytes_received =  recv(client_sock, buff, BUFF_SIZE, 0);
+		buff[bytes_received] = '\0';
+		printf("%s\n", buff);
+		if (strcmp(buff,SEND_FILE_REQUEST) == 0){
+			sendFile(client_sock,filename);
+		}
+
+	}else{
+		printf("%s\n", FILE_NOT_FOUND);
+		bytes_sent = send(client_sock, FILE_NOT_FOUND, BUFF_SIZE, 0);
+	}
+	
+
+}
+
+void doSearchFile(int client_sock){
+	printf("%s\n", "do Search File");
+}
+
+
+void doSendFile(int client_sock){
+	printf("%s\n", "do Send File");
+}
+
+void menu(int client_sock){
+	int a = 0;
+	while(1){
+		printf("%s\n", "=====================");
+		printf("%s\n", "  Menu :");
+		printf("%s\n", "  1.Search Mode ");
+		printf("%s\n", "  2.Share Mode ");
+		printf("Your choice (1 or 2, other to quit): \n");		
+		scanf("%d", &a);
+		if (a>2 || a<1)
+		{
+			printf("%s\n", "Good bye");
+			break;
+		}
+		if (a == 1)
+			searchMode(client_sock);
+		if (a == 2)
+			shareMode(client_sock);
+		
+
+	}
+}
+
+int searchFile(char *filename)
+{
+	FILE *fileptr;
+	char name[100];
+	strcpy(name,STORAGE);
+	strcat(name,filename);
+	if ((fileptr = fopen(name, "rb")) == NULL){
+            return 0;
+        }
+    fclose(fileptr);
+    return 1;
+}
+
+int isValidIpAddress(char const *ipAddress)
+{
+    struct sockaddr_in sa;
+    int result = inet_pton(AF_INET, ipAddress, &(sa.sin_addr));
+    if (result != 0)
+        return 1;
+    else
+        return 0;
+}
+
+int main(int argc, char const *argv[]){
+	
+	if (argc != 3)
+    {
+        printf("error, too many or too few arguments\n");
+        return 1;
+    }
+    //check if input id is valid
+    if (isValidIpAddress(argv[1]) == 0)
+    {
+        printf("Not a valid ip address\n");
+        return 1;
+    }
+
+	int client_sock;
+	char buff[BUFF_SIZE + 1];
+	struct sockaddr_in server_addr; /* server's address information */
+	int msg_len, bytes_sent, bytes_received;
+	
+	//Step 1: Construct socket
+	client_sock = socket(AF_INET,SOCK_STREAM,0);
+	
+	//Step 2: Specify server address
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(atoi(argv[2]));
+    server_addr.sin_addr.s_addr = inet_addr(argv[1]);
+	
+	//Step 3: Request to connect server
+	if(connect(client_sock, (struct sockaddr*)&server_addr, sizeof(struct sockaddr)) < 0){
+		printf("\nError!Can not connect to sever! Client exit imediately! ");
+		return 0;
+	}
+	menu(client_sock);
+
+	close(client_sock);
+	return 0;
+}
---
+++ server.c
@@ -0,0 +1,501 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>       /* These are the usual header files */
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <pthread.h>
+
+
+#define PORT 5550   /* Port that will be opened */ 
+#define BACKLOG 20   /* Number of allowed connections */
+#define BUFF_SIZE 1024
+#define BUFF_SEND 1024
+
+#define FIND_FILE_REQUEST "Find file"	
+#define SEND_FILE_REQUEST "Send file"
+#define FILE_NOT_FOUND "File not found"
+#define FILE_EXIST "File exist"
+
+#define SEARCH_MODE "Search mode"
+#define SHARE_MODE "Share mode"
+#define STORAGE "./save/"
+
+/* Receive and echo message to client */
+
+typedef struct tagAddress
+{
+    char addressIP[256];
+    int *connfd;
+    struct tagAddress *next;
+} address;
+
+typedef struct tagConnfd
+{
+    int connfd;
+    struct tagConnfd *next;
+} linkConn;
+
+address *head = NULL;
+address *current = NULL;
+
+linkConn *headShareList = NULL;
+linkConn *headSearchList = NULL;
+
+void insertListConn(int connfd, linkConn *ptr);
+address* findByConn(int connfd);
+void *mainFunction(void *);
+address *insertListAddr(address addr, address *ptr);
+
+linkConn *deleteNode(linkConn *head_ref, int key) 
+{ 
+    // Store head node 
+    linkConn* temp = head_ref, *prev; 
+  
+    // If head node itself holds the key to be deleted 
+    if (temp != NULL && temp->connfd == key) 
+    { 
+        head_ref = temp->next;   // Changed head 
+        free(temp);               // free old head 
+        return head_ref; 
+    } 
+  
+    // Search for the key to be deleted, keep track of the 
+    // previous node as we need to change 'prev->next' 
+    while (temp != NULL && temp->connfd != key) 
+    { 
+        prev = temp; 
+        temp = temp->next; 
+    } 
+  
+    // If key was not present in linked list 
+    if (temp == NULL) 
+    	return head_ref; 
+  
+    prev->next = temp->next; 
+  
+    free(temp);  // Free memory 
+    return head_ref;
+}
+
+void sendFile(int connfd, char *fileName){
+    
+    FILE *fileptr;
+    long filelen =0l;
+    int bytes_sent;
+    char name[100];
+	strcpy(name,STORAGE);
+	strcat(name,fileName);
+
+    fileptr = fopen(name, "rb");
+    fseek(fileptr, 0, SEEK_END);          // Jump to the end of the file
+    filelen = ftell(fileptr);             // Get the current byte offset in the file       
+    rewind(fileptr);
+
+    printf("%s\n", "Ham gui file");
+
+    bytes_sent = send(connfd, &filelen, 20, 0); //send file size
+    if(bytes_sent <= 0){
+        printf("\nConnection closed!\n");
+        return;
+    }
+
+    int sumByte = 0; //count size byte send
+    printf("\n Bat dau doc file !\n");
+    while(1) {
+        int numberByteSend = BUFF_SIZE;
+        if((sumByte + BUFF_SIZE	) > filelen) {// if over file size
+            numberByteSend = filelen - sumByte; 
+        }
+        char* buffer = (char *) malloc((numberByteSend) * sizeof(char));
+        fread(buffer, numberByteSend, 1, fileptr); // read buffer with size 
+        sumByte += numberByteSend; //increase byte send
+        bytes_sent = send(connfd, buffer, numberByteSend, 0);
+        if(bytes_sent <= 0){
+            printf("\nConnection closed!\n");
+            break;
+        }
+        free(buffer);
+        if(sumByte >= filelen) {
+            break;
+        }
+    }
+    printf("%s\n", "Send file success !");
+    fclose(fileptr); //close file
+}
+
+void receiveFile(int conn, char *fileName){
+	FILE *filePtr;
+	char name[100];
+	char *fileContent;
+	int bytes_received;
+	long filelen =0l;
+	long *size;
+
+	strcpy(name, STORAGE); 
+	strcat(name, fileName);
+
+	printf("%s\n", "Ham nhan file");
+	printf("filelen: %ld\n", filelen);
+
+	size = (long*) malloc(BUFF_SIZE * sizeof(long));
+
+	bytes_received = recv(conn, size, 20, 0);
+	if (bytes_received <= 0){
+		printf("\nConnection closed");
+		return;
+	}
+	filelen = *size;
+	printf("filelen: %ld\n", filelen);
+	printf("Uploaded file: %s, Size of file: %ld\n\n", fileName, filelen);
+
+	int sumByte = 0;
+	filePtr = fopen(name, "wb");
+	fileContent = (char*) malloc(BUFF_SIZE * sizeof(char));
+	while(1) {
+		bytes_received = recv(conn, fileContent, BUFF_SIZE, 0);
+		if(bytes_received == 0) {
+			printf("Error: File tranfering is interupted\n\n");
+		}
+		printf("bytes_received: %d fileContent: %s\n",bytes_received,fileContent );
+		sumByte += bytes_received;
+		fwrite(fileContent, bytes_received, 1, filePtr);
+		printf("%s\n", fileContent);
+		free(fileContent);
+		fileContent = (char*) malloc(BUFF_SIZE * sizeof(char));
+		if(sumByte >= filelen) {
+			break;
+		}
+	}
+	printf("%s\n", "Receive file success !"); 
+	free(fileContent);
+	free(size);		
+	fclose(filePtr);
+}
+
+void sendRequestFindFile(int currentCon, char *fileName){
+	linkConn *pointer;
+    pointer = headShareList;
+    linkConn *headListConnHasFile = NULL;
+    address *headListAddrHasFile = NULL;
+    int connfd;
+    int bytes_sent = 0;
+    int bytes_received = 0;
+    char buff[BUFF_SIZE+1];
+    char listClient[BUFF_SIZE+1];
+    char minus[] = "-----------";
+    char enter[] = "\n";
+    pthread_t tid;
+   	int *connPtr;
+
+
+    address *temp = (address *)malloc(sizeof(address));
+
+	while (pointer != NULL)
+	{
+		printf("pointer: %d\n", pointer->connfd);
+		connfd = pointer->connfd;
+		printf("Send request find file to %d\n", connfd);
+		bytes_sent = send(connfd, fileName, BUFF_SEND, 0);
+		printf("wait for %d\n", connfd);
+		bytes_received = recv(connfd, buff, BUFF_SIZE, 0);
+		printf("bytes_received: %d\n", bytes_received);
+		buff[bytes_received] = '\0';
+		printf("buff: %s\n", buff);
+		if(strcmp(buff,FILE_EXIST)==0){
+			printf("%s\n", FILE_EXIST);
+			temp = findByConn(connfd);
+			headListAddrHasFile = insertListAddr(*temp,headListAddrHasFile);
+
+		}
+		// else{
+		// 	connPtr = &connfd;
+		// 	pthread_create(&tid, NULL, &mainFunction, connPtr);
+		// }
+		pointer = pointer->next;
+
+    }
+    printf("%s : \n",headListAddrHasFile->addressIP );
+    if(headListAddrHasFile == NULL)
+    {
+    	bytes_sent = send(currentCon,FILE_NOT_FOUND, BUFF_SEND, 0);
+    }
+    if (headListAddrHasFile != NULL)
+    {
+    	printf("%s\n", "Send list client !");
+    	address *tmp = headListAddrHasFile;
+    	int i = 0;
+    	char STT[3];
+    	while (tmp != NULL)
+		{
+			i++;
+			connfd = *tmp->connfd;
+			char connStr[20];
+			sprintf(connStr, "%d", connfd);
+			sprintf(STT, "%d", i);
+			strcat(listClient,"Client ");
+			strcat(listClient,STT);
+			strcat(listClient,": IP: ");
+			strcat(listClient,tmp->addressIP);
+			strcat(listClient," -------- ID: ");
+			strcat(listClient,connStr);
+			strcat(listClient,enter);
+			tmp = tmp->next;
+    	}
+
+    	bytes_sent = send(currentCon,listClient, BUFF_SEND, 0);
+    	printf("listClient: %s\n", listClient);
+    	memset(listClient,0,sizeof(listClient));
+    	printf("%s\n", "Sended list client !");
+    	bytes_received = recv(currentCon, buff, BUFF_SIZE, 0);
+    	buff[bytes_received] = '\0';
+    	printf("Client ID has been choosed: %s\n", buff);
+    	connfd = atoi(buff);
+
+    	temp = findByConn(connfd);
+    	printf("Client will get file : %s\n", temp->addressIP);
+
+    	bytes_sent = send(connfd,SEND_FILE_REQUEST, BUFF_SEND, 0);
+
+    	receiveFile(connfd,fileName);
+    	sendFile(currentCon,fileName);
+
+    }
+
+    headShareList = NULL;
+    headSearchList = NULL;
+    address *tmp1 = head;
+    while (tmp1 != NULL)
+	{	
+		pthread_create(&tid, NULL, &mainFunction, tmp1->connfd);
+		tmp1 = tmp1->next;	
+	}
+
+}
+
+void insertListConn(int connfd, linkConn *ptr)
+{
+    linkConn *link = (linkConn *)malloc(sizeof(linkConn));
+    link->connfd = connfd;
+    link->next = ptr;
+    ptr = link;
+}
+
+void insertShareList(int connfd)
+{
+    linkConn *link = (linkConn *)malloc(sizeof(linkConn));
+    link->connfd = connfd;
+    link->next = headShareList;
+    headShareList = link;
+}
+
+void insertSearchList(int connfd)
+{
+    linkConn *link = (linkConn *)malloc(sizeof(linkConn));
+    link->connfd = connfd;
+    link->next = headSearchList;
+    headSearchList = link;
+}
+
+address* findByConn(int connfd){
+   address* current = head;
+   if(head == NULL){
+      return NULL;
+   }
+   while(*current->connfd != connfd){
+
+      if(current->next == NULL){
+         return NULL;
+      }else {
+         current = current->next;
+      }
+   }
+   return current;
+} 
+
+address *insertListAddr(address addr, address *ptr)
+{	
+	printf("%s : %d\n",addr.addressIP,*addr.connfd );
+    address *link = (address *)malloc(sizeof(address));
+    strcpy(link->addressIP, addr.addressIP);
+    link->connfd = (int *)malloc(sizeof(int));
+    *link->connfd = *(addr.connfd);
+    link->next = ptr;
+    ptr = link;
+    printf("%s : %d\n",ptr->addressIP,*ptr->connfd );
+    return ptr;
+}
+
+void InsertHead(address addr)
+{
+    address *link = (address *)malloc(sizeof(address));
+    strcpy(link->addressIP, addr.addressIP);
+    link->connfd = (int *)malloc(sizeof(int));
+    *link->connfd = *(addr.connfd);
+    link->next = head;
+    head = link;
+}
+
+int main(int argc, char const *argv[])
+{ 
+	if (argc != 2)
+    {
+        printf("error, too many or too few arguments\n");
+        printf("Correct format is /.server YourPort\n");
+        return 1;
+    }
+
+	char fileName[BUFF_SIZE];
+	int listenfd, *connfd;
+	struct sockaddr_in server; /* server's address information */
+	struct sockaddr_in *client; /* client's address information */
+	int sin_size;
+	pthread_t tid;
+
+	if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) == -1 ){  /* calls socket() */
+		perror("\nError: ");
+		return 0;
+	}
+	bzero(&server, sizeof(server));
+	server.sin_family = AF_INET;
+    server.sin_port = htons(atoi(argv[1]));     
+    server.sin_addr.s_addr = htonl(INADDR_ANY); 
+	// printf("%s\n", argv[1]);
+	if(bind(listenfd,(struct sockaddr*)&server, sizeof(server))==-1){ 
+		perror("\nError: ");
+		return 0;
+	}     
+
+	if(listen(listenfd, BACKLOG) == -1){  
+		perror("\nError: ");
+		return 0;
+	}
+	
+	sin_size=sizeof(struct sockaddr_in);
+	client = malloc(sin_size);
+	while(1){		
+		connfd = malloc(sizeof(int));
+		sin_size = sizeof(struct sockaddr_in);
+		if ((*connfd = accept(listenfd, (struct sockaddr *)client, &sin_size)) ==- 1)
+			perror("\nError: ");
+				
+		printf("You got a connection from %s\n", inet_ntoa(client->sin_addr) ); /* prints client's IP */
+		address tempAddr;
+		strcpy(tempAddr.addressIP,inet_ntoa(client->sin_addr));
+		// printf(" %d \n", *connfd);
+		tempAddr.connfd = malloc(sizeof(int));
+		tempAddr.connfd = connfd;
+		printf(" %d \n", *(tempAddr.connfd));
+        InsertHead(tempAddr);
+		/* For each client, spawns a thread, and the thread handles the new client */
+		pthread_create(&tid, NULL, &mainFunction, connfd);	
+		printf("%s\n", "aaaaaaaaaaa");
+	}
+	
+	close(listenfd);
+	return 0;
+}
+
+void *mainFunction(void *arg){
+	int connfd;
+	int bytes_sent, bytes_received;
+	char buff[BUFF_SIZE + 1];
+	char fileName[BUFF_SIZE+1];
+	char chooseMode[BUFF_SIZE];
+	int *connPtr;
+	connfd = *((int *) arg);
+	// free(arg);
+	pthread_detach(pthread_self());
+
+	address *ptr;
+    ptr = head;
+
+    printf("NEW %s\n", "===================");
+
+    while (ptr != NULL)
+    {
+    	printf("%s %d \n", ptr->addressIP, *ptr->connfd);
+       	ptr = ptr->next;
+    }
+	
+	bytes_received = recv(connfd, chooseMode, BUFF_SIZE, 0); //blocking
+	if (bytes_received < 0)
+		perror("\nError: ");
+	else if (bytes_received == 0)
+		printf("Connection closed.");
+
+	chooseMode[bytes_received] = '\0';
+
+	if(strcmp(chooseMode,SHARE_MODE) ==0 ){
+		printf("%s\n", "This client choose SHARE_MODE");
+		
+		headShareList = deleteNode(headShareList,connfd);
+		headSearchList = deleteNode(headSearchList,connfd);
+		insertShareList(connfd);
+
+		linkConn *ptr1;
+    	ptr1 = headShareList;
+    	while (ptr1 != NULL)
+    	{
+    		printf("%s %d \n", "Share connfd: ", ptr1->connfd);
+       		ptr1 = ptr1->next;
+    	}
+
+    	ptr1 = headSearchList;
+    	while (ptr1 != NULL)
+    	{
+    		printf("%s %d \n", "Search connfd: ", ptr1->connfd);
+       		ptr1 = ptr1->next;
+    	}
+
+
+	}
+	if(strcmp(chooseMode,SEARCH_MODE) ==0 ){
+		printf("%s\n", "This client choose SEARCH_MODE");
+		
+		headSearchList = deleteNode(headSearchList,connfd);
+		headShareList = deleteNode(headShareList,connfd);
+
+		insertSearchList(connfd);
+
+		
+
+
+		linkConn *ptr1;
+		ptr1 = headSearchList;
+    	while (ptr1 != NULL)
+    	{
+    		printf("%s %d \n", "Search connfd: ", ptr1->connfd);
+       		ptr1 = ptr1->next;
+    	}
+
+
+    	ptr1 = headShareList;
+    	while (ptr1 != NULL)
+    	{
+    		printf("%s %d \n", "Share connfd: ", ptr1->connfd);
+       		ptr1 = ptr1->next;
+    	}
+
+		bytes_received = recv(connfd, fileName, BUFF_SIZE, 0);
+		fileName[bytes_received] = '\0';
+
+
+		printf("File need to find: %s\n", fileName);
+		sendRequestFindFile(connfd,fileName);
+
+		printf("END  %s\n", "================");
+
+
+    	
+
+	}
+	
+	if (bytes_sent < 0){
+		perror("\nError: ");
+	
+	close(connfd);	
+	}
+}
